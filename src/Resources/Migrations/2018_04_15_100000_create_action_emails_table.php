4c6467498e118de2086aa80668b77e63dc711924
Unknown <railken@protonmail.ch>
2018-04-23 22:46:16 +0200

    cleanup

--- src/Resources/Migrations/2018_04_15_100000_create_action_emails_table.php
+++ src/Resources/Migrations/2018_04_15_100000_create_action_emails_table.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Railken\LaraOre\Resources\Migrations;
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
