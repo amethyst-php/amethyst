19fe3d6cbb8650e4315877c3936a7b66e01c9f83
railken <railken@protonmail.ch>
2018-06-01 17:44:28 +0200

    fix

--- tests/Admin/Common/PermissionPolicy.php
+++ tests/Admin/Common/PermissionPolicy.php
@@ -6,11 +6,13 @@ use Railken\Laravel\Manager\Contracts\PolicyContract;
 use Railken\Laravel\Manager\Contracts\AgentContract;
 use Railken\Laravel\Manager\Contracts\ManagerContract;
 
-
 class PermissionPolicy implements PolicyContract
 {
     public function newQuery(ManagerContract $manager, $query)
-    {   
+    {
+        return $query;
+
+        /*
     	$query->leftJoin('ownerships', function($join) {
     		$join->on('ownership.table_name', '=', $query->)
     	});
@@ -24,5 +26,6 @@ class PermissionPolicy implements PolicyContract
         }
 
         throw new \Exception("No role found for user: #".$agent->id);
+        */
     }
 }
